d812b333a4ca9bddcb00c4260e774459a7c714d2
iblug <1115jkb@gmail.com>
2023-02-03 18:40:52 +0900

    2200073_정광배

--- 2200073/3_조교의 성적 매기기.py
+++ 2200073/3_조교의 성적 매기기.py
@@ -1,29 +1,18 @@
 import sys
 sys.stdin = open('input_3.txt', 'r')
 
-# 미완성
 s = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
 T = int(input())
 for t in range(T):
     n, k = map(int, input().split())
-    d = [sum(map(int, input().split())) for _ in range(n)]
-    target = d[k-1]
-    d = sorted(d,reverse=True)
-    rank = d.index(target)+1
-    p = (d.index(target)+1)/(n//10)
-    # d_ = sorted(d)
-    # rank_ = d.index(target)+1
-    # nn = n//10
-    # if rank % nn == 0:
-    #     r = rank//nn -1
-    # else:
-    #     r = rank//nn
-    
-    
-    
-    # nn = (n//10)
-    # r = -(-(d.index(target)+1))//(n//10)
+    d = [list(map(int, input().split())) for _ in range(n)]
 
-    print(f'#{t+1}',rank,n,f'{p:.3}')
-    # print(f'#{t+1}',rank_,n)
-    # print(s[r])
+    # 점수 비중이 다른것을 모르고 삽질하다가 시간을 많이 썼다...
+    d_ = [(i[0]*35 + i[1]*45 + i[2]*20)/100 for i in d]
+
+    target = d_[k-1]
+    d_ = sorted(d_,reverse=True)
+    rank = d_.index(target) + 1
+    g = -(-rank//(n//10)) - 1
+
+    print(f'#{t+1}', s[g])
