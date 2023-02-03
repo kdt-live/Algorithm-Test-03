382781e47530c36b5b6c1c8afaecc99bbe510434
YongJuDo <qud6330@naver.com>
2023-02-03 17:04:01 +0900

    2200006_도용주

---
+++ 2200006/1979.py
@@ -0,0 +1,29 @@
+# 1979번 - 어디에 단어가 들어갈 수 있을까
+T = int(input())
+
+for tc in range(1, T + 1):
+    N, K = map(int, input().split())
+    N_arr = [list(map(int, input().split())) for _ in range(N)]
+    k_len = 0
+    
+    for i in range(N):
+        cnt = 0
+        for j in range(N):
+            if N_arr[i][j] == 1:
+                cnt += 1
+            if N_arr[i][j] == 0 or j == N - 1:
+                if cnt == K:
+                    k_len += 1
+                cnt = 0
+    
+    for i in range(N):
+        cnt = 0
+        for j in range(N):
+            if N_arr[j][i] == 1:
+                cnt += 1
+            if N_arr[j][i] == 0 or j == N - 1:
+                if cnt == K:
+                    k_len += 1
+                cnt = 0
+    
+    print(f'#{tc} {k_len}')
