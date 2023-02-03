62e41fb3684a68ff5de6760d03070a72452e238f
Shureeshu <ruby04.s.lee@gmail.com>
2023-02-03 16:10:55 +0900

    2200078 이수정

---
+++ 2200078/파리퇴치.py
@@ -0,0 +1,23 @@
+def get_x(arr, v):
+    s = sum(sum(arr[i][j] for i in range(v[0], v[0]+M) if i in range(N)) for j in range(v[1], v[1]+M) if j in range(N))
+    return s
+
+def test(N, M):
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    x = max(max(get_x(arr, (i, j)) for j in range(N)) for i in range(N))
+    return x
+
+# :: Testing
+import sys
+sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+inputs = sys.stdin.readlines
+
+
+T = int(input())
+rst = [0]*T
+for _ in range(T):
+    N, M = map(int, input().split())
+    rst[_] = test(N, M)
+
+[print(f'#{i} {rst[i-1]}') for i in range(1, len(rst)+1)]
