11602a417645506b683eacca218beed2385b1bb0
YongDeok Lim <ys03055@naver.com>
2023-02-03 18:02:10 +0900

    알고리즘테스트_3_풀이

---
+++ 파리퇴치.py
@@ -0,0 +1,17 @@
+T = int(input())
+for i in range(1, T+1) :
+    n,m = map(int,input().split())
+    s = []
+    for j in range(n) :
+        a = list(map(int, input().split()))
+        s.append(a)
+    max = 0
+    for x in range(n-m+1) :
+        for y in range(n-m+1) :
+            cnt = 0
+            for z in range(m) :
+                for x in range(m) :
+                    cnt +=s[x+z][y+x]
+                print(cnt)
+            if cnt > max :
+                max = cnt
