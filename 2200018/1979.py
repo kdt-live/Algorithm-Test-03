7cd057a240c2ee4465b3a4ccc3ce33953d7769f9
thebjko <thebjko@gmail.com>
2023-02-06 09:28:36 +0900

    refactor 1979. 어디에 단어가 들어갈 수 있을까

--- 2200018/1979.py
+++ 2200018/1979.py
@@ -1,8 +1,17 @@
 # 어디에 단어가 들어갈 수 있을까
 
 import sys
-sys.stdin = open("input_1983.txt")
+sys.stdin = open("input_1979.txt")
 input = sys.stdin.readline
+
+def count_where(matrix, K: int) -> int:
+    cnt = 0
+    for j in matrix:
+        for k in j.split("0"):
+            if k == "1" * K:
+                cnt += 1
+
+    return cnt
 
 for i in range(int(input())):
     N, K = map(int, input().split())
@@ -10,17 +19,6 @@ for i in range(int(input())):
     for _ in range(N):
         matrix.append(input().strip()[::2])
     
-    cnt = 0
-    for j in matrix:
-        for k in j.split("0"):
-            if k == "1" * K:
-                cnt += 1
-
     transposed = map("".join, zip(*matrix))
-    
-    for j in transposed:
-        for k in j.split("0"):
-            if k == "1" * K:
-                cnt += 1
 
-    print(f"#{i + 1} {cnt}")
+    print(f"#{i + 1} {count_where(matrix, K) + count_where(transposed, K)}")
